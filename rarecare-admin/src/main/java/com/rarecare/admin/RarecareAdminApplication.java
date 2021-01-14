cbfa03d3e182b4629f6e3b7648ec05d2c54dedf3
keunyop <kylee1112@hotmail.com>
2021-01-15 00:19:23 +0900

    initial

---
+++ rarecare-admin/src/main/java/com/rarecare/admin/RarecareAdminApplication.java
@@ -0,0 +1,13 @@
+package com.rarecare.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RarecareAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RarecareAdminApplication.class, args);
+	}
+
+}
